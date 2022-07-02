56301d4243f231489f52edeecfc093557aefa0b3
David Griffiths <davidgg777@hotmail.com>
2022-07-02 16:33:36 +1000

    Initial commit

---
+++ dbt/models/demos/netflix/netflix_data_loading/netflix_copy_into.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='copy_into') }}
+
+select
+    {{ get_stage_columns_from_loading_config(
+        stage_name=source('netflix', 'netflix_blob_stage'), 
+        config_table=ref('loading_config')
+    )}},
+    current_timestamp::timestamp_ntz as ingestion_time
+
+from
+    @{{source('netflix', 'netflix_blob_stage')}}
---
+++ dbt/models/my_first_model.sql
@@ -0,0 +1,3 @@
+{{config(materialized='table')}}
+
+select 'hello, world!' as col 
